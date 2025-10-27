bd7c4c65aa3cd3f7f21778daa9ad59877343572e
Hasaanfarooq <hasaanf987@gmail.com>
2025-10-27 19:49:49 +0500

    Fix: renamed image files and corrected import paths for Netlify build

--- src/components/home/WorldwideProjects.jsx
+++ src/components/home/WorldwideProjects.jsx
@@ -5,8 +5,7 @@ import { Pagination, Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 
-import offerImg from "/src/assets/images/whychooseus/bchoffice.jpg"; // Replace with your actual image paths
-
+import offerImg from "/src/assets/images/product1/containerhallint2.jpg";
 // Project data with route links
 const projects = [
   {
